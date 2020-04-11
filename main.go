c45de51213bc94c8270c541b31a5e6bcb7c97ba1
Mickael <mickael.spilmont.fr@gmail.com>
2020-04-11 15:16:39 +0200

    init screen and display size of terminal

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/gdamore/tcell"
+)
+
+func initScreen() tcell.Screen {
+	screen, err := tcell.NewScreen()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+	if err := screen.Init(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+	screen.EnableMouse()
+
+	return screen
+}
 
 func main() {
-	fmt.Printf("Hello World!")
+	screen := initScreen()
+	width, height := screen.Size()
+	fmt.Printf("Width: %v\nHeight: %v", width, height)
+	// screen.Fini()
 }
