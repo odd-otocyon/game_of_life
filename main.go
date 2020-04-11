300934c6611d53b7b7b8ee47dc27ca9856a89c14
Mickael <mickael.spilmont.fr@gmail.com>
2020-04-11 19:36:30 +0200

    Test with rune

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/gdamore/tcell"
 )
 
+const StyleDefault tcell.Style = 0
+
 func initScreen() tcell.Screen {
 	screen, err := tcell.NewScreen()
 	if err != nil {
@@ -23,8 +25,18 @@ func initScreen() tcell.Screen {
 }
 
 func main() {
+
+	var content string = "Print that"
+	var combc string = "And that"
+	combin := []rune(combc)
+
 	screen := initScreen()
-	width, height := screen.Size()
-	fmt.Printf("Width: %v\nHeight: %v", width, height)
+	screen.Clear()
+	// width, height := screen.Size()
+
+	for x, char := range content {
+		screen.SetContent(x, 1, char, combin, StyleDefault)
+	}
+	screen.Show()
 	// screen.Fini()
 }
